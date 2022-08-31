50933ecf6f4ad16c6e244a7ee0e5f0df0893ed49
Rafael Souza <rafaelrodriguessouza10@gmail.com>
2022-08-31 14:21:39 -0300

    teste maven build

---
+++ src/main/java/com/urusianx/tgitlab/TGitlabApplication.java
@@ -0,0 +1,13 @@
+package com.urusianx.tgitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TGitlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urusianx/tgitlab/TGitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urusianx.tgitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
