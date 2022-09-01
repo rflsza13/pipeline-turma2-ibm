73a5615c810cc0b27d99cb8d7b093a05525d5ef8
Rafael Souza <rafaelrodriguessouza10@gmail.com>
2022-09-01 10:49:05 -0300

    versionando com teste no gitlab

--- src/main/java/com/br/urusianx/RodandoTeste/RodandoTesteApplication.java
+++ src/main/java/com/br/urusianx/RodandoTeste/RodandoTesteApplication.java
@@ -1,13 +1,23 @@
 package com.br.urusianx.RodandoTeste;
 
-import org.springframework.boot.SpringApplication;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RodandoTesteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RodandoTesteApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		System.out.println("Informe o CPF: ");
+
+		BufferedReader reader = new BufferedReader(
+			new InputStreamReader(System.in));
+
+		String cpf = reader.readLine();
+		
+		System.out.println("O CPF digitado é: " + (ValidadorCPF.validar(cpf)));
 	}
 
 }
--- src/test/java/com/br/urusianx/RodandoTeste/RodandoTesteApplicationTests.java
+++ src/test/java/com/br/urusianx/RodandoTeste/RodandoTesteApplicationTests.java
@@ -1,13 +1,22 @@
 package com.br.urusianx.RodandoTeste;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RodandoTesteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void validandoCPFValido() {
+		var verdadeiro = ValidadorCPF.validar("01915078067");
+		assertEquals(true, verdadeiro);
+	}
+
+	@Test
+	void validandoCPFInalido() {
+		var verdadeiro = ValidadorCPF.validar("01915078065");
+		assertEquals(false, verdadeiro);
 	}
 
 }
